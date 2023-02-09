0ef782282462b22b2eadfbb763add9a54bf34fdd
Celiian <90334525+Celiian@users.noreply.github.com>
2023-02-09 15:58:17 +0100

    feat/ dishes display done (ui todo)

--- src/main/java/com/example/gestaurant/controller/client/MenuController.java
+++ src/main/java/com/example/gestaurant/controller/client/MenuController.java
@@ -4,35 +4,44 @@ import com.example.gestaurant.db.DishDb;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import org.bson.Document;
 
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
-import javax.imageio.*;
 public class MenuController implements Initializable {
 
     @FXML
-    private VBox menuBoxLeft;
-
+    private VBox menuBox;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         List<String> dishList = DishDb.getDishes();
 
-        dishList.stream().map(Document::parse)
-                .forEach(dish -> {
-                    System.out.println(dish);
-                    menuBoxLeft.getChildren().addAll(
-                        new Label((String) dish.get("name"))
-                        );
-                });
+        dishList.stream().map(Document::parse).forEach(dish -> {
+            long variable = Long.parseLong(dish.get("price").toString());
+            float price = (float) variable / 100;
+            ArrayList<String> ingredientList = (ArrayList<String>) dish.get("ingredients");
+            String ingredients = ingredientList.stream().map(Object::toString).collect(Collectors.joining(", "));
+            System.out.println(dish.get("status"));
+            if (!Objects.equals((String) dish.get("status"), "out of stock") && !Objects.equals((String) dish.get("status"), "coming soon")) {
+                HBox hbox = new HBox(50);
+                hbox.getChildren().addAll(
+                        new Label((String) dish.get("name")),
+                        new Label(ingredients),
+                        new Label("Prix : " + price + "€")
+                );
+                menuBox.getChildren().addAll(
+                        hbox
+                );
+            }
 
+        });
     }
 }
-
